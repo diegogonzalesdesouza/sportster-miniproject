bab0c38752ef181c2bb9f48740f36878f548f87e
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-18 16:36:53 -0300

    reborn for real

--- app/models/athlete.rb
+++
@@ -1,7 +0,0 @@
-class Athlete < User
-  belongs_to :user
-  validates :first_name, presence: true, length: { minimum: 2 }
-  validates :last_name, presence: true, length: { minimum: 2 }
-  validates :birth_date, presence: true
-  validates :cpf, presence: true
-end
--- app/models/brand.rb
+++
@@ -1,6 +0,0 @@
-class Brand < User
-  belongs_to :user
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :slogan, presence: true, length: { minimum: 2 }
-  validates :cnpj, presence: true
-end
