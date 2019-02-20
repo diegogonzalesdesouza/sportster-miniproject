6516b103eb31752a3c9764fc9eb97bcd178ff860
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-20 16:27:45 -0300

    added like option to brand card

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'devise'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
 gem "pundit"
+gem "font-awesome-rails"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   resources :athletes, except: :index do
     resources :achievements, except: :show
   end
-  resources :interests, except: %i[show index]
+
   get 'all_athletes', to: 'athletes#index'
 
+  resources :interests, except: %i[index show new edit update]
 
   root 'pages#home'
 end
