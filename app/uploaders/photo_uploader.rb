c5931101226eab5617765a39ab39950bca0f0946
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-21 15:07:55 -0300

    brands logo show up on show

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,7 +6,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process convert: 'jpg'
 
   version :thumnail do
-    resize_to_fit 256, 256
+    resize_to_fit 100, 100
   end
 
   version :bright_face do
